2e645fea48ca45a6529a5dcb6ab5665895614618
Fernando Zegada <ferzeg3420@gmail.com>
2021-07-31 00:17:31 -0400

    Commented out all the code for controlling window resizing. Keeping this commit for my records.

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const SCROLL_DURATION = 1500;
+//const SCROLL_DURATION = 1500;
 
 const MIN_SCREEN_WIDTH_FOR_ANIMATIONS = 550;
-const MIN_SCREEN_HEIGHT = 100;
-const OFFSET_HEIGHT = 50;
+//const MIN_SCREEN_HEIGHT = 100;
+//const OFFSET_HEIGHT = 50;
 
-const FIRST_PAGE = 1;
-const SECOND_PAGE = 2;
-const THIRD_PAGE = 3;
+//const FIRST_PAGE = 1;
+//const SECOND_PAGE = 2;
+//const THIRD_PAGE = 3;
 
-const FIRST_ELMN = 0;
-const SECOND_ELMN = 1;
-const THIRD_ELMN = 2;
-
-const SCROLL_UP = 0;
-const SCROLL_DOWN = 0;
+//const FIRST_ELMN = 0;
+//const SECOND_ELMN = 1;
+//const THIRD_ELMN = 2;
+//
+//const SCROLL_UP = 0;
+//const SCROLL_DOWN = 0;
 
 const TYPE_LETTER_DURATION = 120;
 const HALF_A_SECOND = 500;
-const ONE_SECOND = 1000;
+//const ONE_SECOND = 1000;
 const FIVE_SECONDS = 5000;
 
 const INDEX_OF_ANIMATED_ELMN_PARENT = 3;
 const CHILD_INDEX_OF_ANIMATED_ELMN = 1;
 
-let isScrollDisabled = false;
-let isTouchScreen = false;
+//let isScrollDisabled = false;
+//let isTouchScreen = false;
 
 var elementsToAnimateSecondPage =
     document.querySelectorAll(".animate-second-page");
-var elementsToAnimateThirdPage =
-    document.querySelectorAll(".animate-third-page");
+//var elementsToAnimateThirdPage =
+//    document.querySelectorAll(".animate-third-page");
+
+let firstProjectElement = elementsToAnimateSecondPage[0];
 
 if( window.innerHeight > MIN_SCREEN_WIDTH_FOR_ANIMATIONS ) {
     for( let elmn of elementsToAnimateSecondPage ) {
         elmn.classList.add("hidden");
     }
-    for( let elmn of elementsToAnimateThirdPage ) {
-        elmn.classList.add("hidden");
-    }
+//    for( let elmn of elementsToAnimateThirdPage ) {
+//        elmn.classList.add("hidden");
+//    }
 }
 
-var scrollDownKeys = ["ArrowDown", " "];
-var scrollUpKeys = ["ArrowUp"];
+//var scrollDownKeys = ["ArrowDown", " "];
+//var scrollUpKeys = ["ArrowUp"];
 
-var currentPageViewIsOn = FIRST_PAGE;
-var isPageTransitioning = false; 
+//var currentPageViewIsOn = FIRST_PAGE;
+//var isPageTransitioning = false; 
 
 var pageElements = [];
 for( let elmnId of ["first-page", "second-page", "third-page"] ) {
@@ -60,97 +62,97 @@ function animateSecondPage() {
     }
 }
 
-function animateThirdPage() {
-    let windowHeight = window.innerHeight;
+//function animateThirdPage() {
+//    let windowHeight = window.innerHeight;
+//
+//    for( let i = 0; i < elementsToAnimateThirdPage.length; i++ ) {
+//        let element = elementsToAnimateThirdPage[i];
+//        let positionFromTop =
+//            elementsToAnimateThirdPage[i].getBoundingClientRect().top;
+//
+//        if( positionFromTop - windowHeight <= 0 ) {
+//            element.classList.add("fade-in-element");
+//            element.classList.remove("hidden");
+//        }
+//    }
+//}
 
-    for( let i = 0; i < elementsToAnimateThirdPage.length; i++ ) {
-        let element = elementsToAnimateThirdPage[i];
-        let positionFromTop =
-            elementsToAnimateThirdPage[i].getBoundingClientRect().top;
+//function scrollToPage(pageElementIndex, pageIndex) {
+//    pageElements[pageElementIndex].scrollIntoView();
+//    currentPageViewIsOn = pageIndex;
+//}
 
-        if( positionFromTop - windowHeight <= 0 ) {
-            element.classList.add("fade-in-element");
-            element.classList.remove("hidden");
-        }
-    }
-}
+//function isScrollDownKey(keyName) {
+//    for( let downKey of scrollDownKeys ) {
+//        if( keyName == downKey ) {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
 
-function scrollToPage(pageElementIndex, pageIndex) {
-    pageElements[pageElementIndex].scrollIntoView();
-    currentPageViewIsOn = pageIndex;
-}
+//function isScrollUpKey(keyName) {
+//    for( let upKey of scrollUpKeys ) {
+//        if( keyName == upKey ) {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
 
-function isScrollDownKey(keyName) {
-    for( let downKey of scrollDownKeys ) {
-        if( keyName == downKey ) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function isScrollUpKey(keyName) {
-    for( let upKey of scrollUpKeys ) {
-        if( keyName == upKey ) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function transitionBetweenPages(scrollEvent) {
-    // Block/deny transitioning for a bit and do the animation 
-    // for elments appearing on screen when in view
-    if( isPageTransitioning === true ) {
-        return;
-    }
-    isPageTransitioning = true;
-    setTimeout(() => {
-        isPageTransitioning = false; 
-    }, SCROLL_DURATION);
-
-    // actual transitioning between pages 
-    if( currentPageViewIsOn === FIRST_PAGE ) {
-        if( scrollEvent.deltaY > SCROLL_DOWN ) {
-            scrollToPage(SECOND_ELMN, SECOND_PAGE);
-            animateSecondPage(); 
-        }
-        else if( scrollEvent.type === "keydown" ) {
-            if( isScrollDownKey(scrollEvent.key) ) {
-                scrollToPage(SECOND_ELMN, SECOND_PAGE);
-                animateSecondPage(); 
-            }
-        }
-    }
-    else if( currentPageViewIsOn === SECOND_PAGE ) {
-        if( scrollEvent.deltaY < SCROLL_UP ) {
-            scrollToPage(FIRST_ELMN, FIRST_PAGE);
-        }
-        else if( scrollEvent.deltaY > SCROLL_DOWN ) {
-            scrollToPage(THIRD_ELMN, THIRD_PAGE);
-            animateThirdPage(); 
-        }
-        else if( scrollEvent.type === "keydown" ) {
-            if( isScrollUpKey(scrollEvent.key) ) {
-                scrollToPage(FIRST_ELMN, FIRST_PAGE);
-            }
-            else if( isScrollDownKey(scrollEvent.key) ) {
-                scrollToPage(THIRD_ELMN, THIRD_PAGE);
-                animateThirdPage(); 
-            }
-        }
-    }
-    else /* currentViewIsOnThirdPage */ {
-        if( scrollEvent.deltaY < SCROLL_UP ) {
-            scrollToPage(SECOND_ELMN, SECOND_PAGE);
-        }
-        else if( scrollEvent.type === "keydown" ) {
-            if( isScrollUpKey(scrollEvent.key) ) {
-                scrollToPage(SECOND_ELMN, SECOND_PAGE);
-            }
-        }
-    }
-}
+//function transitionBetweenPages(scrollEvent) {
+//    // Block/deny transitioning for a bit and do the animation 
+//    // for elments appearing on screen when in view
+//    if( isPageTransitioning === true ) {
+//        return;
+//    }
+//    isPageTransitioning = true;
+//    setTimeout(() => {
+//        isPageTransitioning = false; 
+//    }, SCROLL_DURATION);
+//
+//    // actual transitioning between pages 
+//    if( currentPageViewIsOn === FIRST_PAGE ) {
+//        if( scrollEvent.deltaY > SCROLL_DOWN ) {
+//            scrollToPage(SECOND_ELMN, SECOND_PAGE);
+//            animateSecondPage(); 
+//        }
+//        else if( scrollEvent.type === "keydown" ) {
+//            if( isScrollDownKey(scrollEvent.key) ) {
+//                scrollToPage(SECOND_ELMN, SECOND_PAGE);
+//                animateSecondPage(); 
+//            }
+//        }
+//    }
+//    else if( currentPageViewIsOn === SECOND_PAGE ) {
+//        if( scrollEvent.deltaY < SCROLL_UP ) {
+//            scrollToPage(FIRST_ELMN, FIRST_PAGE);
+//        }
+//        else if( scrollEvent.deltaY > SCROLL_DOWN ) {
+//            scrollToPage(THIRD_ELMN, THIRD_PAGE);
+//            //animateThirdPage(); 
+//        }
+//        else if( scrollEvent.type === "keydown" ) {
+//            if( isScrollUpKey(scrollEvent.key) ) {
+//                scrollToPage(FIRST_ELMN, FIRST_PAGE);
+//            }
+//            else if( isScrollDownKey(scrollEvent.key) ) {
+//                scrollToPage(THIRD_ELMN, THIRD_PAGE);
+//                //animateThirdPage(); 
+//            }
+//        }
+//    }
+//    else /* currentViewIsOnThirdPage */ {
+//        if( scrollEvent.deltaY < SCROLL_UP ) {
+//            scrollToPage(SECOND_ELMN, SECOND_PAGE);
+//        }
+//        else if( scrollEvent.type === "keydown" ) {
+//            if( isScrollUpKey(scrollEvent.key) ) {
+//                scrollToPage(SECOND_ELMN, SECOND_PAGE);
+//            }
+//        }
+//    }
+//}
 
 // Code to prevent all scrolling
 // courtesy of gblazex on
@@ -158,67 +160,91 @@ function transitionBetweenPages(scrollEvent) {
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1};
+//var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1};
 
-function preventDefaultAndDoCustom(e) {
-    e.preventDefault();
-    transitionBetweenPages(e);
-}
+//function preventDefaultAndDoCustom(e) {
+//    e.preventDefault();
+//    transitionBetweenPages(e);
+//}
 
-function preventDefaultForScrollKeys(e) {
-    if( keys[e.keyCode] ) {
-        preventDefaultAndDoCustom(e);
-        transitionBetweenPages(e);
-        return false;
+//function preventDefaultForScrollKeys(e) {
+//    if( keys[e.keyCode] ) {
+//        preventDefaultAndDoCustom(e);
+//        transitionBetweenPages(e);
+//        return false;
+//    }
+//}
+
+// modern Chrome requires { passive: false } when adding events
+//var supportsPassive = false;
+//try {
+//    window.addEventListener("test", 
+//                            null,
+//                            Object.defineProperty({}, "passive", {
+//        get: function () { supportsPassive = true; }
+//    }));
+//} catch(e) {}
+
+//var wheelOpt = supportsPassive ? { passive: false } : false;
+//var wheelEvent = 
+//    "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+//
+//function disableScroll() {
+//    window.addEventListener(
+//        "DOMMouseScroll",
+//        preventDefaultAndDoCustom, 
+//        false); // older FF
+//    window.addEventListener(
+//        wheelEvent, 
+//        preventDefaultAndDoCustom, 
+//        wheelOpt); // modern desktop
+//    window.addEventListener(
+//        "keydown", 
+//        preventDefaultForScrollKeys, 
+//        false);
+//}
+//
+//function enableScroll() {
+//    window.removeEventListener("DOMMouseScroll",
+//        preventDefaultAndDoCustom,
+//        false);
+//    window.removeEventListener(wheelEvent,
+//        preventDefaultAndDoCustom, 
+//        wheelOpt);
+//    window.removeEventListener("keydown", 
+//        preventDefaultForScrollKeys, 
+//        false);
+//}
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+const projectsAreFullyVisible = window.innerHeight * 0.9;
+
+function showProjectsIfInView(scrollPos) {
+    console.log(scrollPos);
+    let projectPos = firstProjectElement.getBoundingClientRect().top;
+    if( projectPos < projectsAreFullyVisible ) {
+        animateSecondPage();
     }
 }
 
-// modern Chrome requires { passive: false } when adding events
-var supportsPassive = false;
-try {
-    window.addEventListener("test", 
-                            null,
-                            Object.defineProperty({}, "passive", {
-        get: function () { supportsPassive = true; }
-    }));
-} catch(e) {}
+document.addEventListener("scroll", function(e) {
+    lastKnownScrollPosition = window.scrollY;
 
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 
-    "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
-
-function disableScroll() {
-    window.addEventListener(
-        "DOMMouseScroll",
-        preventDefaultAndDoCustom, 
-        false); // older FF
-    window.addEventListener(
-        wheelEvent, 
-        preventDefaultAndDoCustom, 
-        wheelOpt); // modern desktop
-    window.addEventListener(
-        "keydown", 
-        preventDefaultForScrollKeys, 
-        false);
-}
-
-function enableScroll() {
-    window.removeEventListener("DOMMouseScroll",
-        preventDefaultAndDoCustom,
-        false);
-    window.removeEventListener(wheelEvent,
-        preventDefaultAndDoCustom, 
-        wheelOpt);
-    window.removeEventListener("keydown", 
-        preventDefaultForScrollKeys, 
-        false);
-}
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            showProjectsIfInView(lastKnownScrollPosition);
+            ticking = false;
+        });
+        ticking = true;
+    }
+});
 
 document.getElementById("second-page-link")
     .addEventListener("click",
         (e) => {
             if( ! (e.metaKey || e.ctrlKey) ) {
-                currentPageViewIsOn = SECOND_PAGE;
+                //currentPageViewIsOn = SECOND_PAGE;
                 animateSecondPage(); 
             }
         });
@@ -227,9 +253,9 @@ document.getElementById("third-page-link")
     .addEventListener("click",
         (e) => {
             if( ! (e.metaKey || e.ctrlKey) ) {
-                currentPageViewIsOn = THIRD_PAGE;
+                //currentPageViewIsOn = THIRD_PAGE;
                 animateSecondPage(); 
-                animateThirdPage(); 
+                //animateThirdPage(); 
             }
         });
 
@@ -259,35 +285,35 @@ function hideOneAndShowAnother(elmnIdToHide, elmnIdToShow, delay) {
     }, delay);
 }
 
-if( window.innerWidth > MIN_SCREEN_WIDTH_FOR_ANIMATIONS ) {
-    disableScroll();
-    isScrollDisabled = true;
-}
-else {
-    for( let elmn of elementsToAnimateSecondPage ) {
-        elmn.classList.remove("hidden");
-    }
-    for( let elmn of elementsToAnimateThirdPage ) {
-        elmn.classList.remove("hidden");
-    }
-}
+//if( window.innerWidth > MIN_SCREEN_WIDTH_FOR_ANIMATIONS ) {
+//    disableScroll();
+//    isScrollDisabled = true;
+//}
+//else {
+//    for( let elmn of elementsToAnimateSecondPage ) {
+//        elmn.classList.remove("hidden");
+//    }
+//    for( let elmn of elementsToAnimateThirdPage ) {
+//        elmn.classList.remove("hidden");
+//    }
+//}
 
-window.onresize = () => {
-    if( window.innerWidth <= MIN_SCREEN_WIDTH_FOR_ANIMATIONS
-        && isScrollDisabled ) 
-    {
-        enableScroll();
-        isScrollDisabled = false;
-    }
-    else if( window.innerWidth > MIN_SCREEN_WIDTH_FOR_ANIMATIONS
-             && (! isScrollDisabled) )
-    {
-        disableScroll();
-        if(! isTouchScreen ) {
-            scrollToPage(FIRST_ELMN, FIRST_PAGE);
-        }
-    }
-};
+//window.onresize = () => {
+//    if( window.innerWidth <= MIN_SCREEN_WIDTH_FOR_ANIMATIONS
+//        && isScrollDisabled ) 
+//    {
+//        enableScroll();
+//        isScrollDisabled = false;
+//    }
+//    else if( window.innerWidth > MIN_SCREEN_WIDTH_FOR_ANIMATIONS
+//             && (! isScrollDisabled) )
+//    {
+//        disableScroll();
+//        if(! isTouchScreen ) {
+//            scrollToPage(FIRST_ELMN, FIRST_PAGE);
+//        }
+//    }
+//};
 
 let areProjectAnimationsBlocked = {
     "first": false,
@@ -319,40 +345,40 @@ for( let number of ["first", "second", "third", "fourth", "fifth"]) {
     });
 }
 
-function handleOpenOnNewTab() {
-    if (document.visibilityState === "visible") {
-        setTimeout( () => {
-            if( window.scrollY > MIN_SCREEN_HEIGHT 
-                && window.scrollY <= (window.innerHeight + OFFSET_HEIGHT) ) {
-                scrollToPage(SECOND_ELMN, SECOND_PAGE);
-                animateSecondPage(); 
-            }
-            else if( window.scrollY > (window.innerHeight + MIN_SCREEN_HEIGHT) )
-            {
-                scrollToPage(THIRD_ELMN, THIRD_PAGE);
-                animateSecondPage(); 
-            }
-            document.removeEventListener("visibilitychange",
-                                         handleOpenOnNewTab);
-        }, ONE_SECOND);
-    }
-}
+//function handleOpenOnNewTab() {
+//    if (document.visibilityState === "visible") {
+//        setTimeout( () => {
+//            if( window.scrollY > MIN_SCREEN_HEIGHT 
+//                && window.scrollY <= (window.innerHeight + OFFSET_HEIGHT) ) {
+//                scrollToPage(SECOND_ELMN, SECOND_PAGE);
+//                animateSecondPage(); 
+//            }
+//            else if( window.scrollY > (window.innerHeight + MIN_SCREEN_HEIGHT) )
+//            {
+//                scrollToPage(THIRD_ELMN, THIRD_PAGE);
+//                animateSecondPage(); 
+//            }
+//            document.removeEventListener("visibilitychange",
+//                                         handleOpenOnNewTab);
+//        }, ONE_SECOND);
+//    }
+//}
 
-document.addEventListener("touchstart", () => {
-    isTouchScreen = true;
-    if( isScrollDisabled ) {
-        isScrollDisabled = false;
-        enableScroll();
-    }
-    for( let elmn of elementsToAnimateSecondPage ) {
-        elmn.classList.remove("hidden");
-    }
-    for( let elmn of elementsToAnimateThirdPage ) {
-        elmn.classList.remove("hidden");
-    }
-});
+//document.addEventListener("touchstart", () => {
+//    isTouchScreen = true;
+//    if( isScrollDisabled ) {
+//        isScrollDisabled = false;
+//        enableScroll();
+//    }
+//    for( let elmn of elementsToAnimateSecondPage ) {
+//        elmn.classList.remove("hidden");
+//    }
+//    for( let elmn of elementsToAnimateThirdPage ) {
+//        elmn.classList.remove("hidden");
+//    }
+//});
 
-document.addEventListener("visibilitychange", handleOpenOnNewTab);
+//document.addEventListener("visibilitychange", handleOpenOnNewTab);
 
 document.addEventListener("DOMContentLoaded",
     () => { 
